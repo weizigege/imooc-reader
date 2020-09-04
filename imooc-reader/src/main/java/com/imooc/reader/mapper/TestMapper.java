d4711fe81559e8bfe7471178fbe7289fa719be2e
weizi <1952253119@qq.com>
2020-09-04 21:06:00 +0800

    开发到index的分页

--- imooc-reader/src/main/java/com/imooc/reader/mapper/TestMapper.java
+++ imooc-reader/src/main/java/com/imooc/reader/mapper/TestMapper.java
@@ -1,11 +1,14 @@
 package com.imooc.reader.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.imooc.reader.entity.Test;
+
 /**
  * @program: imooc-reader
  * @description
  * @author: hu
  * @create: 2020-09-03 16:50
  **/
-public interface TestMapper {
-    public void insert();
+public interface TestMapper extends BaseMapper<Test> {
+    public void insertSample();
 }
--- imooc-reader/src/main/java/com/imooc/reader/service/TestService.java
+++ imooc-reader/src/main/java/com/imooc/reader/service/TestService.java
@@ -2,6 +2,7 @@ package com.imooc.reader.service;
 
 import com.imooc.reader.mapper.TestMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
@@ -15,9 +16,14 @@ import javax.annotation.Resource;
 public class TestService {
     @Resource
     private TestMapper testMapper;
+
+    @Transactional
     public void batchImport(){
         for (int i = 0; i < 5; i++) {
-            testMapper.insert();
+            if (i==3){
+                throw new RuntimeException("出现异常");
+            }
+            testMapper.insertSample();
         }
     }
 
